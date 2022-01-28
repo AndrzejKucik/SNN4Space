e5cce33274dddc00b4dc6030e8c0f0eace981c94
AndrzejKucik <andrzej.kucik@gmail.com>
2022-01-28 11:50:59 +0100

    simplify

--- train_models.py
+++ train_models.py
@@ -1,247 +1,85 @@
-#!/usr/bin/env python3.6
+#!/usr/bin/env python3.10
+# -*- coding: utf-8 -*-
 
-"""Fine tuning of the VGG16 network on the UC Merced or EuroSAT datasets."""
-
-# -- Built-in modules -- #
-import datetime
-import os
-from argparse import ArgumentParser
-from pathlib import Path
-
-# -- Third-party modules -- #
-import tensorflow as tf
-
-# -- Proprietary modules -- #
-from create_models import create_vgg16_model
-from dataloaders import load_ucm, load_eurosat
-from utils import augment, input_filter_map, rescale_resize
+"""Fine-tuning of the VGG16 network on the UC Merced or EuroSAT datasets."""
 
 # -- File info -- #
 __author__ = 'Andrzej S. Kucik'
 __copyright__ = 'European Space Agency'
 __contact__ = 'andrzej.kucik@esa.int'
-__version__ = '0.3.3'
-__date__ = '2021-03-04'
+__version__ = '0.4.0'
+__date__ = '2022-01-28'
 
-# - Argument parser - #
-parser = ArgumentParser()
-# -- Dataset
-parser.add_argument('-ds',
-                    '--dataset',
-                    required=True,
-                    type=str,
-                    default='eurosat',
-                    help='Dataset. Either `eurosat` or `ucm`. One can also add `prewitt`, `sobel`, `mask` or `sq`.')
-# -- Seed
-parser.add_argument('-s',
-                    '--seed',
-                    type=int,
-                    default=5,
-                    help='Global random seed.')
-# -- Training parameters
-parser.add_argument('-e',
-                    '--epochs',
-                    type=int,
-                    default=1000,
-                    help='Number of training epochs.')
-parser.add_argument('-bs',
-                    '--batch_size',
-                    type=int,
-                    default=32,
-                    help='Batch size (per replica).')
-parser.add_argument('-lr',
-                    '--learning_rate',
-                    type=float,
-                    default=0.001,
-                    help='Learning rate.')
-# -- Model parameters
-parser.add_argument('-kl2',
-                    '--kernel_l2',
-                    type=float,
-                    default=1e-4,
-                    help='Regularization L2 parameter for the convolutional kernels.')
-parser.add_argument('-bl1',
-                    '--bias_l1',
-                    type=float,
-                    default=1e-5,
-                    help='Regularization L1 parameter for the convolutional biases.')
-# -- Augmentation parameters
-parser.add_argument('-lz',
-                    '--lower_zoom',
-                    type=float,
-                    default=.95,
-                    help='Augmentation parameter. Lower bound for a random zoom factor. Must be positive.')
-parser.add_argument('-uz',
-                    '--upper_zoom',
-                    type=float,
-                    default=1.05,
-                    help='Augmentation parameter. Upper bound for a random zoom factor. '
-                         + 'Must be bigger than lower_zoom.')
-parser.add_argument('-mbd',
-                    '--max_brightness_delta',
-                    type=float,
-                    default=.2,
-                    help='Augmentation parameter. Maximum brightness delta. Must be a non-negative float.')
-parser.add_argument('-mhd',
-                    '--max_hue_delta',
-                    type=float,
-                    default=.1,
-                    help='Augmentation parameter. Maximum hue delta. Must be in the interval [0, .5].')
-parser.add_argument('-lc',
-                    '--lower_contrast',
-                    type=float, default=.2,
-                    help='Augmentation parameter. Lower bound for a random contrast factor. Must be positive.')
-parser.add_argument('-uc',
-                    '--upper_contrast',
-                    type=float,
-                    default=1.8,
-                    help='Augmentation parameter. Upper bound for a random contrast factor. '
-                         + 'Must be bigger than lower_contrast.')
-parser.add_argument('-ls',
-                    '--lower_saturation',
-                    type=float,
-                    default=.9,
-                    help='Augmentation parameter. Lower bound for a random saturation factor. Must be positive.')
-parser.add_argument('-us',
-                    '--upper_saturation',
-                    type=float,
-                    default=1.1,
-                    help='Augmentation parameter. Upper bound for a random saturation factor. '
-                         + 'Must be bigger than lower_saturation.')
+# -- Built-in modules -- #
+import sys
 
-# - Parse arguments
-args = vars(parser.parse_args())
-# -- Dataset
-DATASET = args['dataset'].lower()
-# -- Seed
-SEED = args['seed']
-# -- Training parameters
-EPOCHS = args['epochs']
-BATCH_PER_REPLICA = args['batch_size']
-LR = args['learning_rate']
-# -- Regularization parameters
-KERNEL_L2 = args['kernel_l2']
-BIAS_L1 = args['bias_l1']
+# -- Third-party modules -- #
+import tensorflow as tf
 
-# Fix the dataset parameters
-if 'eurosat' in DATASET:
-    INPUT_SHAPE = (64, 64, 3)
-    NUM_CLASSES = 10
-    BUFFER_SIZE = 21600
-elif 'ucm' in DATASET:
-    INPUT_SHAPE = (224, 224, 3)
-    NUM_CLASSES = 21
-    BUFFER_SIZE = 1680
-else:
-    exit('Invalid dataset!')
+# -- Proprietary modules -- #
+from argument_parser import parse_arguments
+from create_models import create_vgg16_model
+from dataloaders import AUGMENTATION_PARAMETERS, load_data
+from utils import colour_str
 
-# Set the seed for reproducibility
-tf.random.set_seed(seed=SEED)
+if __name__ == '__main__':
+    # Get the arguments
+    args = parse_arguments(arguments=sys.argv[1:])
 
-# Strategy parameters (for multiple GPU training) #
-STRATEGY = tf.distribute.MirroredStrategy(cross_device_ops=tf.distribute.HierarchicalCopyAllReduce())
-NUM_DEVICES = STRATEGY.num_replicas_in_sync
-print('\nNumber of devices: {}\n'.format(NUM_DEVICES))
+    # Set the seed for reproducibility
+    tf.random.set_seed(seed=args['seed'])
 
-# Global batch size #
-BATCH_SIZE = BATCH_PER_REPLICA * NUM_DEVICES
+    # Strategy parameters (for multiple GPU training) #
+    strategy = tf.distribute.MirroredStrategy(cross_device_ops=tf.distribute.HierarchicalCopyAllReduce())
+    num_devices = strategy.num_replicas_in_sync
+    print(f"Number of devices: {colour_str(num_devices, 'purple')}")
 
-# Model filepath #
-MODEL_FILEPATH = Path('models/vgg16').joinpath(DATASET)
-os.makedirs(MODEL_FILEPATH, exist_ok=True)
-MODEL_FILEPATH = MODEL_FILEPATH.joinpath('s_{}'.format(SEED)
-                                         + '_e_{}'.format(EPOCHS)
-                                         + '_bs_{}'.format(BATCH_SIZE)
-                                         + '_lr_{}'.format(LR)
-                                         + '_kl2_{}'.format(KERNEL_L2)
-                                         + '_bl1_{}'.format(BIAS_L1)
-                                         + '_lz_{}'.format(args['lower_zoom'])
-                                         + '_uz_{}'.format(args['upper_zoom'])
-                                         + '_mbd_{}'.format(args['max_brightness_delta'])
-                                         + '_mhd_{}'.format(args['max_hue_delta'])
-                                         + '_lc_{}'.format(args['lower_contrast'])
-                                         + '_uc_{}'.format(args['upper_contrast'])
-                                         + '_ls_{}'.format(args['lower_saturation'])
-                                         + '_us_{}.h5'.format(args['upper_saturation']))
+    # Global batch size
+    batch_size = args['batch_size'] * num_devices
 
-
-# Main
-def main():
-    """The main function."""
-
-    # Prepare the data for training
-    # - Load data
-    if 'eurosat' in DATASET:
-        x_train, x_val, x_test, _ = load_eurosat()
-    else:  # 'ucm' in DATASET
-        x_train, x_val, x_test, _ = load_ucm()
-
-    # - Resize and rescale the images, and cache the training and the validation sets for faster training
-    x_train = x_train.map(rescale_resize(image_size=INPUT_SHAPE[:-1]),
-                          num_parallel_calls=tf.data.experimental.AUTOTUNE).cache()
-    x_val = x_val.map(rescale_resize(image_size=INPUT_SHAPE[:-1]),
-                      num_parallel_calls=tf.data.experimental.AUTOTUNE).cache()
-    x_test = x_test.map(rescale_resize(image_size=INPUT_SHAPE[:-1]), num_parallel_calls=tf.data.experimental.AUTOTUNE)
-
-    # - Shuffle the training set and apply the augmentation (after caching to avoid caching randomness)
-    x_train = x_train.shuffle(BUFFER_SIZE).map(augment(image_size=INPUT_SHAPE[:-1], augmentation_parameters=args),
-                                               num_parallel_calls=tf.data.experimental.AUTOTUNE)
-
-    # - Batch data
-    x_train = x_train.batch(BATCH_SIZE)
-    x_val = x_val.batch(BATCH_SIZE)
-    x_test = x_test.batch(BATCH_SIZE)
-
-    # - Optional gradient-based input (Prewitt and Sobel filters must be applied after batching)
-    x_train = x_train.map(input_filter_map(filter_name=DATASET), num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    x_val = x_val.map(input_filter_map(filter_name=DATASET), num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    x_test = x_test.map(input_filter_map(filter_name=DATASET), num_parallel_calls=tf.data.experimental.AUTOTUNE)
-
-    # - Prefetch data
-    x_train = x_train.prefetch(tf.data.experimental.AUTOTUNE)
-    x_val = x_val.prefetch(tf.data.experimental.AUTOTUNE)
-    x_test = x_test.prefetch(tf.data.experimental.AUTOTUNE)
+    # Load data
+    augmentation_parameters = {key: args[key] for key in AUGMENTATION_PARAMETERS.keys()}
+    train, val, test, _ = load_data(dataset=args['dataset'],
+                                    input_size=args['input_shape'][:-1],
+                                    augmentation_parameters=augmentation_parameters,
+                                    batch_size=batch_size)
 
     # The model
-    with STRATEGY.scope():
-        # - Create a model
-        model = create_vgg16_model(input_shape=INPUT_SHAPE,
-                                   kernel_l2=KERNEL_L2,
-                                   bias_l1=BIAS_L1,
-                                   num_classes=NUM_CLASSES,
+    with strategy.scope():
+        # - Create model
+        model = create_vgg16_model(input_shape=args['input_shape'],
+                                   kernel_l2=args['kernel_l2'],
+                                   bias_l1=args['bias_l1'],
+                                   num_classes=args['num_classes'],
                                    remove_pooling=False,
                                    use_dense_bias=False)
 
-        # - Compile the model
-        model.compile(optimizer=tf.keras.optimizers.RMSprop(LR),
+        # -- Compile the model
+        model.compile(optimizer=tf.keras.optimizers.RMSprop(args['learning_rate']),
                       loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
                       metrics=tf.metrics.SparseCategoricalAccuracy())
 
-    # - Show the summary of the model
+    # -- Show the summary of the model
     model.summary()
 
     # Callbacks
-    log_dir = 'logs/fit/' + datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
-    callbacks = [tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1),
+    callbacks = [tf.keras.callbacks.TensorBoard(log_dir=f"logs/{args['model_name']}/fit", histogram_freq=1),
                  tf.keras.callbacks.ReduceLROnPlateau(verbose=True, patience=50),
                  tf.keras.callbacks.EarlyStopping(patience=100),
-                 tf.keras.callbacks.ModelCheckpoint(filepath=MODEL_FILEPATH, verbose=True, save_best_only=True)]
+                 tf.keras.callbacks.ModelCheckpoint(filepath=args['model_path'], verbose=True, save_best_only=True)]
 
     # Train the model
-    model.fit(x=x_train, epochs=EPOCHS, validation_data=x_val, callbacks=callbacks)
+    model.fit(x=train, epochs=args['epochs'], validation_data=val, callbacks=callbacks)
 
     # Load the best weights
-    model.load_weights(MODEL_FILEPATH)
+    model.load_weights(args['model_path'])
 
     # Evaluate the model
-    loss, acc = model.evaluate(x=x_test)
-    print('\nBest model\'s accuracy: {:.2f}%.\n'.format(acc * 100))
+    loss, acc = model.evaluate(x=test)
+    print("Best model's accuracy:", colour_str(f'{acc:.2%}', 'green'))
 
-    # Rename the best model file to include the accuracy in its name
-    save_path = MODEL_FILEPATH.with_name(MODEL_FILEPATH.stem + '_acc_{:.2f}.h5'.format(acc * 100))
-    print('\nSaving the model to:' + str(save_path))
-    os.rename(MODEL_FILEPATH, save_path)
-
-
-if __name__ == '__main__':
-    main()
+    # Log the evaluation results to Tensorboard
+    summary_writer = tf.summary.create_file_writer(f"logs/{args['model_name']}/evaluate")
+    with summary_writer.as_default():
+        tf.summary.scalar('Test loss', loss, step=args['epochs'])
+        tf.summary.scalar('Test accuracy', acc, step=args['epochs'])
